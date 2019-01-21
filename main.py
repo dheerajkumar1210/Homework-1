6671d156250b27898caffce8e4e0bf6057afc435
dheerajkumar1210 <dheeraj.kumar7@gmail.com>
2019-01-21 23:02:06 +0530

    Homework #1
    Homework #1, commit 1

---
+++ main.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 21 22:42:32 2019
+By executing the below code, one get to know description of my fav.
+song.
+The details will be like:
+song
+artist
+poet
+released date 
+genre
+duration
+album name
+audio company
+@author: dheeraj_kumar
+"""
+
+#song name
+song = "Be-Sabab Baat"
+
+#singer
+artist = "Jagjit Singh"
+
+#lyrics by
+poet = "Shahid Kabir"
+
+#date published
+released_date = 1994
+
+#genre
+genre = "ghazal"
+
+#time duration
+duration = 7.15
+
+#album
+album_name = "Face to Face"
+
+#Released by
+audio_company = "Megnasound"
+
+print(song)
+print(artist)
+print(poet)
+print(released_date)
+print(genre)
+print(duration)
+print(album_name)
+print(audio_company)
